7095349421eb5e5f0435908938f79ddc93561dab
egmkang wang <egmkang@outlook.com>
2021-07-29 15:34:06 +0800

    reduce VSCode Pylance warnings

--- fastapi_serviceutils/app/logger.py
+++ fastapi_serviceutils/app/logger.py
@@ -25,7 +25,7 @@ class _InterceptHandler(logging.Handler):
 
         # Find caller from where originated the logging call
         frame, depth = logging.currentframe(), 2
-        while frame.f_code.co_filename == logging.__file__:
+        while frame and frame.f_code.co_filename == logging.__file__:
             frame = frame.f_back
             depth += 1
 
